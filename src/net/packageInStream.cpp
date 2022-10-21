f94c49afbd917dabb4f9a69450bbd42845d68079
shecannotsee <shecannotsee@163.com>
2022-10-21 17:23:16 +0800

    1.buffer设计

---
+++ src/net/packageInStream.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shecannotsee on 2022/10/21.
+//
+
+#include "packageInStream.h"
---
+++ src/net/packageInStream.h
@@ -0,0 +1,16 @@
+//
+// Created by shecannotsee on 2022/10/21.
+//
+
+#ifndef DEVICEENROLLMENTCLOUDSERVICE_SRC_NET_PACKAGEINSTREAM_H_
+#define DEVICEENROLLMENTCLOUDSERVICE_SRC_NET_PACKAGEINSTREAM_H_
+
+class packageInStream {
+ private:
+ public:
+  packageInStream();
+  ~packageInStream();
+ public:
+};
+
+#endif //DEVICEENROLLMENTCLOUDSERVICE_SRC_NET_PACKAGEINSTREAM_H_
