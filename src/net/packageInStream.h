95b5def7db0178973d9b9f49fa5eb1ede00757f1
shecannotsee <shecannotsee@163.com>
2022-10-26 14:06:42 +0800

    1.调整字节流的带头拆分分包结构

--- src/net/packageInStream.h
+++ src/net/packageInStream.h
@@ -11,13 +11,14 @@
 
 class packageInStream {
  public:
+  enum {HEADER=1,DATA=2,DATAEND=3};
   struct header {
     unsigned int flag   : 1;// 0-数据结尾的包
     unsigned int length : 31;// 1-数据长度
   };
   struct half_data {
-    int type;// 0-干净的缓冲区,1-header,2-just data
-    int length;// type对应长度
+    int type;// 0-干净的缓冲区,1-header,2-just data,3-是数据末尾
+    int length;// type对应长度,表示从下一个流中要读取的数据长度
     std::string content;// 实际数据存放位置
   };
  private:
