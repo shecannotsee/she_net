3f823562d5857900aceba7cd31b04fd7e627a4cd
shecannotsee <shecannotsee@163.com>
2022-10-27 17:28:34 +0800

    docs:添加了epoll的封装文档

--- src/net/Epoll.h
+++ src/net/Epoll.h
@@ -15,6 +15,7 @@ class Epoll {
   using message = std::tuple<bool,std::string>;
  private:
   int _epoll_container_id;
+  // 从内核态拷贝到用户态的事件列表
   std::vector<epoll_event> _events;
   int _max_events;
   message _info;
@@ -25,11 +26,13 @@ class Epoll {
   ~Epoll();
  public:
   // call epoll_wait
-  int aliveEvents();
+  int aliveEvents(int timeout);
   // call epoll_ctl
-  void addEvent(int socket_id,int operation);
-  void removeEvent() = delete;
-  void modifyEvent() = delete;
+  // operation : 添加事件 EPOLL_CTL_ADD,移除事件 EPOLL_CTL_DEL,修改事件 EPOLL_CTL_MOD
+  // setEvent : EPOLLIN, EPOLLOUT, EPOLLET
+  void modifyEvent(int socket_id,int operation, int setEvent);
+  // get event[index],在使用epoll_wait(this->aliveEvents)后使用
+  // 返回(活跃)事件的fd以及事件类型( EPOLLIN or EPOLLOUT )
   std::tuple<int, uint32_t> getEventInfo(int index) const;
   message getInfo() const;
 
