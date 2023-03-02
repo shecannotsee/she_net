955b9efe5a57d6c16d07ea2df0432149346d5248
shecannotsee <shecannotsee@163.com>
2023-03-02 16:47:12 +0800

    feat:添加异常类

---
+++ src/sheNetException/sheNetException.cpp
@@ -0,0 +1,18 @@
+//
+// Created by shecannotsee on 2023/2/27.
+//
+
+#include "sheNetException.h"
+
+sheNet::sheNetException::sheNetException(int error_code, const std::string& error_message)
+    : error_code_(error_code),
+      error_message_(std::move(std::string("sheNet error["+std::to_string(error_code_)+"]"+error_message+"\n"))) {
+};
+
+const char *sheNet::sheNetException::what() const noexcept {
+  return error_message_.c_str();
+};
+
+int sheNet::sheNetException::getErrorCode() const {
+  return error_code_;
+}
---
+++ src/sheNetException/sheNetException.h
@@ -0,0 +1,31 @@
+//
+// Created by shecannotsee on 2023/3/2.
+//
+
+#ifndef SHE_NET_SRC_SHENETEXCEPTION_SHENETEXCEPTION_H_
+#define SHE_NET_SRC_SHENETEXCEPTION_SHENETEXCEPTION_H_
+
+#include <exception>
+#include <string>
+
+namespace sheNet {
+
+class sheNetException : public std::exception {
+ public:
+  sheNetException(int error_code,
+                  const std::string &error_message);
+
+ private:
+  int error_code_;
+  std::string error_message_;
+
+ public:
+  virtual const char *what() const noexcept override;
+
+  int getErrorCode() const;
+
+};// class sheNetException
+
+};// namespace sheNet
+
+#endif //SHE_NET_SRC_SHENETEXCEPTION_SHENETEXCEPTION_H_
