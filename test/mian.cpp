4abf3b905ac81e308c985911992ffdc98a37bb91
shecannotsee <shecannotsee@163.com>
2023-03-03 11:09:27 +0800

    test:添加

--- test/mian.cpp
+++ test/mian.cpp
@@ -4,11 +4,12 @@
 #include <iostream>
 
 #include "m1_host_byte_order_test.h"
+#include "m2_base_socket_test.h"
 
 int main() {
   std::cout<<"test start.\n";
   m1_host_byte_order_test::main();
-
+  m2_base_socket_test::main();
   std::cout<<"Done.\n";
   return 0;
 };
