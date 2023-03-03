6b06954ebd04248cadd042e69dc66ede00b1b098
shecannotsee <shecannotsee@163.com>
2023-03-03 11:09:44 +0800

    feat:添加网络层和传输层的设置

---
+++ src/net_base.h
@@ -0,0 +1,20 @@
+//
+// Created by shecannotsee on 2023/3/3.
+//
+
+#ifndef SHE_NET_SRC_NET_BASE_H_
+#define SHE_NET_SRC_NET_BASE_H_
+
+namespace sheNet {
+
+enum class network_transport : unsigned char {
+  TCP_IPV4,
+  TCP_IPV6,
+  UDP_IPV4,
+  UDP_IPV6
+};
+
+
+};// namespace sheNet
+
+#endif //SHE_NET_SRC_NET_BASE_H_
