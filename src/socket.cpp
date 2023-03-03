2164afaeef8b5a35c9c788c77d64011e9f9f9f91
shecannotsee <shecannotsee@163.com>
2023-03-03 11:37:05 +0800

    feat:实现初始化

--- src/socket.cpp
+++ src/socket.cpp
@@ -5,6 +5,7 @@
 #include "socket.h"
 #include <sheNetException/sheNetException.h>
 
+#include <netinet/in.h>
 #include <sys/socket.h>
 
 sheNet::socket::socket()
@@ -13,13 +14,13 @@ sheNet::socket::socket()
 
 sheNet::socket::socket(sheNet::NetTransport type) noexcept{
   if (type==NetTransport::TCP_IPV4) {
-    id_ = ::socket(AF_INET);
+    id_ = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   } else if (type==NetTransport::TCP_IPV6) {
-
+    id_ = ::socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
   } else if (type==NetTransport::UDP_IPV4) {
-
+    id_ = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
   } else if (type==NetTransport::UDP_IPV6) {
-
+    id_ = ::socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
   }
   if (id_ == -1) {
     throw sheNetException(1,"socket create error.");
