12bd4fd82fd9a95ffa95834b6ffe94650f225b6d
shecannotsee <shecannotsee@163.com>
2023-03-03 11:08:53 +0800

    test:添加基础socket测试框架

---
+++ test/m2_base_socket_test.h
@@ -0,0 +1,18 @@
+//
+// Created by shecannotsee on 2023/3/3.
+//
+
+#ifndef SHE_NET_TEST_M2_BASE_SOCKET_TEST_H_
+#define SHE_NET_TEST_M2_BASE_SOCKET_TEST_H_
+
+namespace m2_base_socket_test {
+
+int main() {
+
+
+  return 0;
+};
+
+};// namespace m2_base_socket_test
+
+#endif //SHE_NET_TEST_M2_BASE_SOCKET_TEST_H_
