1cc10d197d2c27ba98ae0b574c3524118216293e
shecannotsee <shecannotsee@163.com>
2023-03-03 15:02:18 +0800

    feat:添加c++11的make unique

---
+++ src/make_unique.h
@@ -0,0 +1,23 @@
+//
+// Created by shecannotsee on 2023/2/27.
+//
+
+#ifndef SHE_NET_SRC_MAKE_UNIQUE_H_
+#define SHE_NET_SRC_MAKE_UNIQUE_H_
+
+#include <memory>
+
+namespace sheNet {
+
+namespace CPP11 {
+
+template<typename T, typename... Ts>
+std::unique_ptr<T> make_unique(Ts &&... params) {
+  return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
+};
+
+};// namespace CPP11 {
+
+};// namespace sheNet
+
+#endif //SHE_NET_SRC_MAKE_UNIQUE_H_
