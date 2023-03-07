ca2aa506f5d3a37c200af3773a624167af20f5ef
shecannotsee <shecannotsee@163.com>
2023-03-07 18:18:40 +0800

    test:添加tcp读写和upd读写的测试框架

---
+++ test/m3_tcp_io_test.h
@@ -0,0 +1,18 @@
+//
+// Created by shecannotsee on 2023/3/7.
+//
+
+#ifndef SHE_NET_TEST_M3_TCP_IO_TEST_H_
+#define SHE_NET_TEST_M3_TCP_IO_TEST_H_
+
+namespace m3_tcp_io_test {
+
+int main() {
+
+
+  return 0;
+};
+
+};// namespace m3_tcp_io_test
+
+#endif //SHE_NET_TEST_M3_TCP_IO_TEST_H_
---
+++ test/m4_udp_io_test.h
@@ -0,0 +1,18 @@
+//
+// Created by shecannotsee on 2023/3/7.
+//
+
+#ifndef SHE_NET_TEST_M4_UDP_IO_TEST_H_
+#define SHE_NET_TEST_M4_UDP_IO_TEST_H_
+
+namespace m4_udp_io_test {
+
+int main() {
+
+
+  return 0;
+};
+
+};// namespace m4_udp_io_test
+
+#endif //SHE_NET_TEST_M4_UDP_IO_TEST_H_
