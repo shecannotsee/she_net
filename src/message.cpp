96b5930f0446fdfe454fdd0d0deb24b8db24b34c
shecannotsee <shecannotsee@163.com>
2023-03-07 18:11:36 +0800

    feat:划分整体发送和接受架构

--- src/message.cpp
+++ src/message.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "message.h"
+#include <sheNetException/sheNetException.h>
+
+sheNet::message::message(std::unique_ptr<sheNet::socket> socket)
+    : socket_(socket.release()) {
+};
+
+std::string sheNet::message::get() {
+  if (socket_->get_net_transport()==NetTransport::TCP_IPV4 || socket_->get_net_transport()==NetTransport::TCP_IPV6) {
+    this->tcp_get();
+  }
+  else if (socket_->get_net_transport()==NetTransport::TCP_IPV4 || socket_->get_net_transport()==NetTransport::TCP_IPV6) {
+    this->udp_get();
+  }
+};
+
+void sheNet::message::send(std::string&& str) {
+  if (socket_->get_net_transport()==NetTransport::TCP_IPV4 || socket_->get_net_transport()==NetTransport::TCP_IPV6) {
+    this->tcp_send();
+  }
+  else if (socket_->get_net_transport()==NetTransport::TCP_IPV4 || socket_->get_net_transport()==NetTransport::TCP_IPV6) {
+    this->udp_send();
+  }
+};
+
+void sheNet::message::tcp_get() {
+
+};
+
+void sheNet::message::tcp_send() {
+
+};
+
+void sheNet::message::udp_get() {
+
+};
+
+void sheNet::message::udp_send() {
+
+};
--- src/message.h
+++ src/message.h
@@ -6,23 +6,34 @@
 #define SHE_NET_SRC_MESSAGE_H_
 
 #include "socket.h"
+#include <string>
+#include <memory>
 
 namespace sheNet {
 
 class message {
  public:
   message() = delete;
-  message(int);
+  message(std::unique_ptr<sheNet::socket> socket);
   ~message() = default;
-  // copy
-  message(const message& x) = default;
-  message& operator=(const message& x) = default;
-  // move
+  // disable copy
+  message(const message& x) = delete;
+  message& operator=(const message& x) = delete;
+  // open move
   message(message&&) = default;
   message& operator=(message&& x) = default;
  private:
-
+  std::unique_ptr<sheNet::socket> socket_;
  public:
+  std::string get();
+  void send(std::string&&);
+
+ private:
+  void tcp_get();
+  void tcp_send();
+  void udp_get();
+  void udp_send();
+
 
 
 
