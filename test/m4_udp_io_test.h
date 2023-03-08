f00a12befbf1f6f874e4c246b85e622b24c75a13
shecannotsee <shecannotsee@163.com>
2023-03-08 11:22:01 +0800

    feat:添加upd读写测试框架

--- test/m4_udp_io_test.h
+++ test/m4_udp_io_test.h
@@ -5,6 +5,18 @@
 #ifndef SHE_NET_TEST_M4_UDP_IO_TEST_H_
 #define SHE_NET_TEST_M4_UDP_IO_TEST_H_
 
+#include <iostream>
+#include <string>
+#include <future>
+#include <memory>
+
+#include <socket.h>
+#include <message.h>
+#include <net_transport.h>
+#include <make_unique.h>
+
+#include <unistd.h>
+
 namespace m4_udp_io_test {
 
 int main() {
