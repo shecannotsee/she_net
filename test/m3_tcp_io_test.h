a89333032e8ea3be4642bdec9bf064232b94f022
shecannotsee <shecannotsee@163.com>
2023-03-08 11:07:04 +0800

    feat:添加tcp读写测试

--- test/m3_tcp_io_test.h
+++ test/m3_tcp_io_test.h
@@ -5,11 +5,62 @@
 #ifndef SHE_NET_TEST_M3_TCP_IO_TEST_H_
 #define SHE_NET_TEST_M3_TCP_IO_TEST_H_
 
+#include <iostream>
+#include <string>
+#include <future>
+#include <memory>
+
+#include <socket.h>
+#include <message.h>
+#include <net_transport.h>
+#include <make_unique.h>
+
+#include <unistd.h>
+
 namespace m3_tcp_io_test {
 
 int main() {
+  auto server = []() {
+    try {
+      sheNet::NetTransport tcp = sheNet::NetTransport::TCP_IPV4;
+      sheNet::socket server_fd(tcp);
+      server_fd.bind("9981");
+      server_fd.listen();
+      sheNet::ClientInfo client_info = server_fd.accept();
+      std::unique_ptr<sheNet::socket> client_fd = sheNet::CPP11::make_unique<sheNet::socket>(tcp);
+      client_fd->set_id(*client_info.fd_);
+      sheNet::message message_control(std::move(client_fd));
+      std::cout << "server set done.\n";
+      while (1) {
+        std::string data = message_control.get();
+        std::cout << "data is:[" << data << "]\n";
+        sleep(1);
+      }
+    } catch (const std::exception& exc) {
+      std::cout<<exc.what()<<std::endl;
+    }
+  };
 
+  auto client = []() {
+    try {
+      sheNet::NetTransport tcp = sheNet::NetTransport::TCP_IPV4;
+      std::unique_ptr<sheNet::socket> client_fd = sheNet::CPP11::make_unique<sheNet::socket>(tcp);
+      client_fd->connect("192.168.1.65", "9981");
+      sheNet::message message_control(std::move(client_fd));
+      std::cout << "client set done.\n";
+      while (1) {
+        message_control.send("hello world");
+        sleep(2);
+      }
+    } catch (const std::exception& exc) {
+      std::cout<<exc.what()<<std::endl;
+    }
+  };
 
+  auto result1 = std::async(std::launch::async,server);
+  auto result2 = std::async(std::launch::async,client);
+
+  sleep(100);
   return 0;
 };
 
--- test/main.cpp
+++ test/main.cpp
@@ -5,11 +5,15 @@
 
 #include "m1_host_byte_order_test.h"
 #include "m2_base_socket_test.h"
+#include "m3_tcp_io_test.h"
+#include "m4_udp_io_test.h"
 
 int main() {
   std::cout<<"test start.\n";
-  m1_host_byte_order_test::main();
-  m2_base_socket_test::main();
+//  m1_host_byte_order_test::main();
+//  m2_base_socket_test::main();
+  m3_tcp_io_test::main();
+//  m4_udp_io_test::main();
   std::cout<<"Done.\n";
   return 0;
 };
