3bec0ba8e9ed442c5b95ada7b836f488c84c501b
shecannotsee <shecannotsee@163.com>
2023-03-08 16:46:10 +0800

    test:添加测试udp发送代码

--- test/m4_udp_io_test.h
+++ test/m4_udp_io_test.h
@@ -20,16 +20,45 @@
 namespace m4_udp_io_test {
 
 int main() {
-  auto server = []() {
+  std::string ip = "192.168.1.47";
+  std::string port = "9981";
+
+  auto client = [&]() {
     try {
+      sheNet::NetTransport udp = sheNet::NetTransport::UDP_IPV4;
+      std::unique_ptr<sheNet::socket> client = sheNet::CPP11::make_unique<sheNet::socket>(udp);/* upd client init */ {
+        sheNet::quadruple quadruple;
+        quadruple.destination_ip = ip;
+        quadruple.destination_port = std::atoi(port.c_str());
+        client->udp_set(std::move(quadruple));
+      };
+      sheNet::message message_control(std::move(client));
+      std::cout << "udp client set done.\n";
+      while (1) {
+        message_control.send("hello world");
+        std::cout << "send done.\n";
+        sleep(2);
+      }
 
     } catch (const std::exception& exc) {
       std::cout<<exc.what()<<std::endl;
     }
   };
 
-  auto client = []() {
+  auto server = [&]() {
     try {
+      sheNet::NetTransport udp = sheNet::NetTransport::UDP_IPV4;
+      std::unique_ptr<sheNet::socket> server = sheNet::CPP11::make_unique<sheNet::socket>(udp);/* upd server init */ {
+        server->bind(port);
+      };
+      sheNet::message message_control(std::move(server));
+      std::cout << "udp server set done.\n";
+      while (1) {
+        std::cout << "wait to data...\n";
+        std::string data = message_control.get();
+        std::cout << "data is:[" << data << "]\n";
+        sleep(1);
+      }
 
     } catch (const std::exception& exc) {
       std::cout<<exc.what()<<std::endl;
@@ -39,6 +68,7 @@ int main() {
   auto server_future = std::async(std::launch::async,server);
   auto client_future = std::async(std::launch::async,client);
 
+  sleep(100);
   return 0;
 };
 
