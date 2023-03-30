350fa52328982f5dda922b3342aa3da0fcc9bffb
shecannotsee <shecannotsee@163.com>
2023-03-30 17:03:41 +0800

    feat:添加获取可使用的fd,并且在取走该fd后会将其io准备状态改为false,以用来等待下一次io

--- src/connection_pool.cpp
+++ src/connection_pool.cpp
@@ -4,6 +4,12 @@
 
 #include "connection_pool.h"
 
+int sheNet::connection_pool::get_open() {
+  connection_pool_[open_socket_fd_] = false;
+  return open_socket_fd_;
+};
+
+
 void sheNet::connection_pool::set_open(int socket_id) {
   connection_pool_[socket_id] = true;
 };
--- src/connection_pool.h
+++ src/connection_pool.h
@@ -1,6 +1,10 @@
 //
 // Created by shecannotsee on 2023/3/24.
 //
+// 设计该类的初衷在于管理多个已经建立连接的socket fd
+// 并且在map结构中,永远都只有一个fd是open的,也就是connection_pool_[fd_number]为true
+// 考虑到在取走fd以后,可能会对fd进行读写,所以同一时间只允许一个fd设置为open,这样能够保证fd能够及时处理
+//
 
 #ifndef SHE_NET_SRC_CONNECTION_POOL_H_
 #define SHE_NET_SRC_CONNECTION_POOL_H_
@@ -23,6 +27,9 @@ class connection_pool {
  private:
   std::unordered_multimap<int,bool> connection_pool_;
   int open_socket_fd_;
+
+ public:
+  int get_open();
 
  public:
   // io ready
