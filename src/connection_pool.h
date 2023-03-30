9896de06ac87e81f195e4a062926029153ce41d3
shecannotsee <shecannotsee@163.com>
2023-03-30 16:48:44 +0800

    fix:涉及到socket描述符的时候应该禁用copy

--- src/connection_pool.h
+++ src/connection_pool.h
@@ -13,20 +13,23 @@ class connection_pool {
  public:
   connection_pool();
   ~connection_pool();
-  // copy
-  connection_pool(const connection_pool& x) = default;
-  connection_pool& operator=(const connection_pool& x) = default;
+  // disable
+  connection_pool(const connection_pool& x) = delete;
+  connection_pool& operator=(const connection_pool& x) = delete;
   // move
   connection_pool(connection_pool&&) = default;
   connection_pool& operator=(connection_pool&& x) = default;
 
  private:
   std::unordered_multimap<int,bool> connection_pool_;
+  int open_socket_fd_;
 
  public:
+  // io ready
   void set_open(int socket_id);
 
  public:
+  // just allow to accept() to add
   void add(int socket);
   void remove(int socket);
 
