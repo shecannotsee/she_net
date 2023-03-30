4ceae7319a4519770c2b3d21e41d06a52bdad0f0
shecannotsee <shecannotsee@163.com>
2023-03-30 18:07:36 +0800

    test:添加tcp双向通讯的例子

--- test/m8_tcp_bidirectional_io_test.h
+++ test/m8_tcp_bidirectional_io_test.h
@@ -20,49 +20,50 @@
 namespace m8_tcp_bidirectional_io_test {
 
 int main() {
-  std::string ip = "192.168.1.47";
-  std::string port = "9981";
-
-  auto server = [&]() {
+  auto server_recv_and_send = [](const std::string& local_port,const std::string& connect_port,int flag) {
     try {
       sheNet::NetTransport tcp = sheNet::NetTransport::TCP_IPV4;
-      std::unique_ptr<sheNet::socket> server = sheNet::CPP11::make_unique<sheNet::socket>(tcp);/* tcp init */{
-        server->bind(port);
+      std::unique_ptr<sheNet::socket> server = sheNet::CPP11::make_unique<sheNet::socket>(tcp);
+      std::unique_ptr<sheNet::socket> client = sheNet::CPP11::make_unique<sheNet::socket>(tcp);
+      if (flag==0) {
+        server->bind(local_port);
         server->listen();
         server->accept();
-      };
-      sheNet::message message_control(std::move(server));
-      std::cout << "server set done.\n";
-      while (1) {
-        std::string data = message_control.get();
-        std::cout << "data is:[" << data << "]\n";
-        sleep(1);
+        client->connect("192.168.1.47",connect_port);
+      } else if (flag == 1) {
+        client->connect("192.168.1.47",connect_port);
+        server->bind(local_port);
+        server->listen();
+        server->accept();
       }
+
+      sheNet::message message_control_server(std::move(server));
+      sheNet::message message_control_client(std::move(client));
+
+      auto send_thread = std::async(std::launch::async,[&](){
+        while (1) {
+          message_control_client.send("1");
+          std::cout<<"send to "<<connect_port << "success.\n";
+          sleep(1);
+        }
+      });
+      auto recv_thread = std::async(std::launch::async,[&](){
+        while (1) {
+          std::string data = message_control_server.get();
+          std::cout << "data is:[" << data << "]\n";
+          sleep(1);
+        }
+      });
     } catch (const std::exception& exc) {
       std::cout<<exc.what()<<std::endl;
     }
   };
 
-  auto client = [&]() {
-    try {
-      sheNet::NetTransport tcp = sheNet::NetTransport::TCP_IPV4;
-      std::unique_ptr<sheNet::socket> client = sheNet::CPP11::make_unique<sheNet::socket>(tcp);/* tcp init */ {
-        client->connect(ip, port);
-      };
-      sheNet::message message_control(std::move(client));
-      std::cout << "client set done.\n";
-      while (1) {
-        message_control.send("hello world");
-        sleep(2);
-      }
-    } catch (const std::exception& exc) {
-      std::cout<<exc.what()<<std::endl;
-    }
-  };
-
-  auto server_future = std::async(std::launch::async,server);
-  auto client_future = std::async(std::launch::async,client);
-  sleep(100);
+  // 绑定本机的9981端口进行监听;去连接9982端口,然后向该端口发送数据
+  auto server_future = std::async(std::launch::async,server_recv_and_send,"9981","9982",0);
+  // 绑定本机的9982端口进行监听;去连接9981端口,然后向该端口发送数据
+  auto client_future = std::async(std::launch::async,server_recv_and_send,"9982","9981",1);
+//  sleep(100);
   return 0;
 };
 
