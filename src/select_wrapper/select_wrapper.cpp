b668246fcc3e1cf6cfeee41425c9a178fd4e25df
shecannotsee <shecannotsee@163.com>
2023-05-17 15:20:34 +0800

    [feat] 1.添加获取select监听的最大描述符的接口 2.添加 select_wrapper 中对连接描述列表添加的功能 3.添加 select_wrapper 中对连接描述列表删除的功能

--- src/select_wrapper/select_wrapper.cpp
+++ src/select_wrapper/select_wrapper.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "select_wrapper.h"
+
+#include <string.h>
+#include <string>
+#include <algorithm>
+
+#include <sys/socket.h>
+#include <sys/resource.h>
+
+#include <sheNetException/sheNetException.h>
+
+sheNet::select_wrapper::select_wrapper() {
+  FD_ZERO(&read_fds_);
+  FD_ZERO(&write_fds_);
+  FD_ZERO(&except_fds_);
+  // 只增大其容量到最大监听描述符+1
+  fd_list_.reserve(sheNet::select_wrapper::get_system_max_listen_fd() +1 );
+};
+
+int sheNet::select_wrapper::get_system_max_listen_fd() {
+  rlimit rlim{};
+  if (getrlimit(RLIMIT_NOFILE, &rlim) == 0) {
+    return static_cast<int>(rlim.rlim_cur);
+  } else {
+    throw sheNetException(14,"get select max value error:"+std::string(strerror(errno)));
+  }
+};
+
+void sheNet::select_wrapper::add_alive_fd(int fd) {
+  fd_list_.push_back(fd);
+  FD_SET(fd, &read_fds_);
+};
+
+void sheNet::select_wrapper::remove_alive_fd(int fd) {
+  auto it = std::remove(fd_list_.begin(), fd_list_.end(), fd);
+  fd_list_.erase(it);
+};
+
+
+
+
+
--- src/select_wrapper/select_wrapper.h
+++ src/select_wrapper/select_wrapper.h
@@ -5,6 +5,9 @@
 #ifndef SHE_NET_SRC_SELECT_SELECT_WRAPPER_H_
 #define SHE_NET_SRC_SELECT_SELECT_WRAPPER_H_
 
+#include <vector>
+#include <sys/socket.h>
+
 namespace sheNet {
 
 class select_wrapper {
@@ -12,17 +15,41 @@ class select_wrapper {
   // copy:off
   select_wrapper(const select_wrapper&) = delete;
   select_wrapper& operator=(const select_wrapper&) = delete;
-  // move:off
-  select_wrapper(select_wrapper&&) = delete;
-  select_wrapper& operator=(select_wrapper&&) = delete;
+  // move:on
+  select_wrapper(select_wrapper&&) = default;
+  select_wrapper& operator=(select_wrapper&&) = default;
   //destructors
   ~select_wrapper() = default;
   //constructors
-  select_wrapper() = default;
+  select_wrapper();
+
+ private:// data
+  fd_set read_fds_;
+  fd_set write_fds_;
+  fd_set except_fds_;
+
+  std::vector<int> fd_list_;
+
  private:
-  // data
- public:
-  // interface
+  /**
+   * @brief 获取select监听的描述符的最大值(软件限制)用于select接口的第一个参数
+   * @return 返回当前(软件层面)限制的最大值
+   */
+  static int get_system_max_listen_fd();
+
+ public:// interface
+  /**
+   * @brief 向select_wrapper所维护的fd列表中添加通过basic_socket_operations::accept获取的fd列表.
+   * 含义其实就是将已经连接上的fd放在一个列表中维护,后续通过该列表进行io处理
+   * @param fd 需要添加的文件描述符
+   */
+  void add_alive_fd(int fd);
+
+  /**
+   * @brief 去除已经断开连接的fd
+   * @param fd 需要删除的文件描述符
+   */
+  void remove_alive_fd(int fd);
 
 };
 
