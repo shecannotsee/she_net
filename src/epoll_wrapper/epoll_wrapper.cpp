cca0ea48edd5aa176dd1a077e24d5e00f9f2d6f9
shecannotsee <shecannotsee@163.com>
2023-05-18 00:32:13 +0800

    [feat] 1.添加了移除epoll event的操作 2.修改了其他一些接口的命名空间,改为全局空间

--- src/epoll_wrapper/epoll_wrapper.cpp
+++ src/epoll_wrapper/epoll_wrapper.cpp
@@ -19,7 +19,7 @@ sheNet::epoll_wrapper::epoll_wrapper(int events_num)
     : user_events_(events_num),
       timeout_set_(-1),
       server_socket_fd_(-1) {
-  epoll_container_id_ = epoll_create(1);
+  epoll_container_id_ = ::epoll_create(1);
 };
 
 void sheNet::epoll_wrapper::add_server_fd(int local_fd) {
@@ -30,8 +30,20 @@ void sheNet::epoll_wrapper::add_alive_fd(int fd ,EPOLL_EVENTS trigger_mode) cons
   epoll_event event_options{};
   event_options.events  = EPOLLIN | trigger_mode;
   event_options.data.fd = fd;
-  int control_result = epoll_ctl(epoll_container_id_,EPOLL_CTL_ADD,server_socket_fd_,&event_options);
+  int control_result = ::epoll_ctl(epoll_container_id_,EPOLL_CTL_ADD,server_socket_fd_,&event_options);
   if (control_result == -1) {
     throw sheNetException(17,"epoll_ctl add system interface error:"+std::string(strerror(errno)));
+  }
+};
+
+void sheNet::epoll_wrapper::remove_adlive_fd(int fd) {
+  epoll_event event_options{};
+  event_options.data.fd = fd;
+
+  int control_result =
+          ::epoll_ctl(epoll_container_id_, EPOLL_CTL_DEL, server_socket_fd_, &event_options);
+  // 调用 epoll_ctl，使用 EPOLL_CTL_DEL 操作将客户端的文件描述符从 epoll 实例中剔除
+  if (control_result == -1) {
+    throw sheNetException(18,"epoll_ctl add system interface error:"+std::string(strerror(errno)));
   }
 };
