3c415b3bb7a750c34449c13fcb5d9e1acf1af4b0
shecannotsee <shecannotsee@163.com>
2023-05-18 18:48:11 +0800

    [test]添加udp的测试客户端和服务端,服务端在运行的时候会有bug,需要修复

--- test/m11_basic_io_operations_udp_test.h
+++ test/m11_basic_io_operations_udp_test.h
@@ -5,17 +5,76 @@
 #ifndef SHE_NET_M11_BASIC_IO_OPERATIONS_UDP_TEST_H
 #define SHE_NET_M11_BASIC_IO_OPERATIONS_UDP_TEST_H
 
+#include <thread>
+#include <basic_socket_operations/basic_socket_operations.h>
 #include <basic_io_operations/basic_io_operations.h>
 
 namespace m11_basic_io_operations_udp_test {
 
+const std::string RESET_COLOR   = "\033[0m";
+const std::string RED_COLOR     = "\033[31m";
+const std::string GREEN_COLOR   = "\033[32m";
+const std::string YELLOW_COLOR  = "\033[33m";
+
 void main() {
-  /* udp io */ {
-    using udp_io = sheNet::basic_io_operations::UDP;
-    int fd_udp = 0;
-    std::string udp_message = udp_io::recvfrom(fd_udp);
-    udp_io::sendto(fd_udp,"","","");
-  }
+  std::cout << RED_COLOR << "basic_io_operations udp test start." << RESET_COLOR << std::endl;
+
+  auto client = std::thread([](){
+    /* create socket */
+    using BSO = sheNet::basic_socket_operations;
+    int client_fd = BSO::socket(sheNet::TRANSPORT_ADDRESS_TYPE::UDP_IPV4);
+
+    /* io */
+    using io = sheNet::basic_io_operations::UDP;
+    while (true) {
+      try {
+        static int message_num = 0;
+        io::sendto(client_fd, "192.168.1.47", "9981", "No." + std::to_string(++message_num) + " message has been sent.");
+        std::cout << YELLOW_COLOR << "No." + std::to_string(message_num) + " message has been sent.\n" << RESET_COLOR;
+        sleep(1);
+      }
+      catch (const sheNet::sheNetException& exc) {
+        std::cout << YELLOW_COLOR << exc.what() << RESET_COLOR;
+        break;
+      }
+    }
+
+    BSO::shutdown(client_fd);
+    std::cout << YELLOW_COLOR << "udp client closed.\n" << RESET_COLOR;
+  });
+
+  auto server = std::thread([](){
+    sleep(2);
+    /* create socket */
+    using BSO = sheNet::basic_socket_operations;
+    auto UDP_IPV4 = sheNet::TRANSPORT_ADDRESS_TYPE::UDP_IPV4;
+    int server_fd = BSO::socket(UDP_IPV4);/* set */ {
+      BSO::port_reuse(server_fd);
+      BSO::set_socket_noblock(server_fd);
+      BSO::bind(server_fd,"0.0.0.0","9981",UDP_IPV4);
+    };
+
+    /* io */
+    using io = sheNet::basic_io_operations::UDP;
+    while (true) {
+      try {
+        std::string message_get = io::recvfrom(server_fd);
+        if (message_get.size() > 0) {
+          std::cout << GREEN_COLOR << "[" << message_get << "]\n" << RESET_COLOR;
+        }
+        sleep(1);
+      }
+      catch (const sheNet::sheNetException& exc) {
+        std::cout << GREEN_COLOR << exc.what() << "\n" << RESET_COLOR;
+        break;
+      }
+    }
+    BSO::shutdown(server_fd);
+    std::cout << GREEN_COLOR << "udp server closed.\n" << RESET_COLOR;
+  });
+
+  client.join();
+  server.join();
 
 };
 
--- test/main.cpp
+++ test/main.cpp
@@ -23,14 +23,14 @@ int main() {
 //  m1_host_byte_order_test::main();
 //  m2_base_socket_test::main();
 //  m3_tcp_io_test::main();
-//  m4_udp_io_test::main();
+  m4_udp_io_test::main();
 //  m5_select_test::main();
 //  m6_poll_test::main();
 //  m7_epoll_test::main();
 //  m8_tcp_bidirectional_io_test::main();
 //  m9_basic_socket_operations::main();
 //  m10_basic_io_operations_tcp_test::main();
-  m11_basic_io_operations_udp_test::main();
+//  m11_basic_io_operations_udp_test::main();
 //  m12_select_wrapper_test::main();
 //  m13_poll_wrapper_test::main();
 //  m14_epoll_wrapper_test::main();
