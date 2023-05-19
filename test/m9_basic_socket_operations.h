100c049b0c50410347f1df578251f27541b193c4
shecannotsee <shecannotsee@163.com>
2023-05-19 17:39:14 +0800

    [test]修改格式更易于阅读

--- test/m9_basic_socket_operations.h
+++ test/m9_basic_socket_operations.h
@@ -18,18 +18,18 @@ void main() {
   std::thread server([]() {
     auto type = sheNet::TRANSPORT_ADDRESS_TYPE::TCP_IPV4;
     sheNet::four_tuple address;/* init */ {
-    address.source_ip = "0.0.0.0";
-    address.source_port = 9981;
-  }
+      address.source_ip = "0.0.0.0";
+      address.source_port = 9981;
+    };
     int fd = st::socket(type);/* print */{
-    std::cout << "server:socket create fd:" << fd << "\n";
-  };
+      std::cout << "server:socket create fd:" << fd << "\n";
+    };
     st::bind(fd, address.source_ip, std::to_string(address.source_port), type);/* print */ {
-    std::cout << "server:bind " + address.source_ip + ":" + std::to_string(address.source_port) + " success.\n";
-  };
+      std::cout << "server:bind " + address.source_ip + ":" + std::to_string(address.source_port) + " success.\n";
+    };
     st::listen(fd);/* print */ {
-    std::cout << "server:listen success.\n";
-  };
+      std::cout << "server:listen success.\n";
+    };
     /* print */ {
       std::cout << "server:wait to accept...\n";
     }
@@ -38,7 +38,8 @@ void main() {
         int client_fd = st::accept(fd);/* print */ {
           std::cout << "server:accept to client fd is " << client_fd << "\n";
         }
-      } catch (sheNet::sheNetException exc) {
+      }
+      catch (const sheNet::sheNetException& exc) {
         std::cout << exc.what();
         break;
       }
@@ -60,7 +61,8 @@ void main() {
         used_port = st::connect(fd, "192.168.1.47", "9981");/* print */ {
           std::cout << "client:connect success,local port is[" + std::to_string(used_port) + "]\n";
         };
-      } catch (sheNet::sheNetException exc) {
+      }
+      catch (const sheNet::sheNetException& exc) {
         std::cout << exc.what();
         break;
       };
