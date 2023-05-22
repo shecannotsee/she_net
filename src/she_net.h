bc7e99c190ddc9f424d5957e4748c42ec27d5768
shecannotsee <shecannotsee@163.com>
2023-05-22 15:57:49 +0800

    [feat]添加包含库所有的文件的头文件

--- src/she_net.h
+++ src/she_net.h
@@ -5,4 +5,32 @@
 #ifndef SHE_NET_SRC_SHE_NET_H_
 #define SHE_NET_SRC_SHE_NET_H_
 
+#include <make_unique.h>
+#include <sheNetException/sheNetException.h>
+
+#include <transport_address_type.h>
+#include <four_tuple.h>
+#include <basic_io_operations/basic_io_operations.h>
+#include <basic_socket_operations/basic_socket_operations.h>
+
+#include <select_wrapper/select_wrapper.h>
+#include <poll_wrapper/poll_wrapper.h>
+#include <epoll_wrapper/epoll_wrapper.h>
+
+#include <basic_tcp/tcp_client.h>
+#include <basic_tcp/tcp_server.h>
+#include <basic_udp/udp_client.h>
+#include <basic_udp/udp_server.h>
+
+#include <stream_data_parsing/base64.h>
+#include <stream_data_parsing/json.h>
+#include <stream_data_parsing/xml.h>
+#include <stream_data_parsing/length_prefixed_messages.h>
+#include <stream_data_parsing/fixed_length_messages.h>
+// will drop
+#include <high_coupling_model/socket.h>
+#include <high_coupling_model/message.h>
+#include <stream_data_parsing/packageInStream.h>
+#include <stream_data_parsing/packageInStreamBase64.h>
+
 #endif //SHE_NET_SRC_SHE_NET_H_
